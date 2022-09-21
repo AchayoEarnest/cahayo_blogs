8f8c6290734dbdde6b77087651a76996c87618d7
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-09-21 16:45:12 +0300

    fixed a bug in aricle model statuses

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,7 @@ class Article < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true, length: { minimum: 10 }
 
-    VALID_STATUSES = ['public', 'private', 'aechived']
+    VALID_STATUSES = ['public', 'private', 'archived']
 
     validates :status, inclusion: { in: VALID_STATUSES }
 
